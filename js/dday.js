3f61c1729c9b044ec8118859fd383f4f7759f478
Inseop Kim <opnayn@gmail.com>
2017-05-26 13:58:40 +0000

    js: dday: Update

--- js/dday.js
+++ js/dday.js
@@ -4,13 +4,10 @@
 		dday = new Date('Sep 7, 2017'), //전역일
 		ytime = 24 * 60 * 60 * 1000, // 1년을 ms로 변환
 		r_dday = Math.ceil((dday.getTime() - today.getTime()) / ytime), // 남은 기간
-		a_dday = Math.ceil((dday.getTime() - sday.getTime()) / ytime); // 총 복무일
+		a_dday = Math.ceil((dday.getTime() - sday.getTime()) / ytime), // 총 복무일
+		per = Math.round(((a_dday - r_dday) / a_dday) * 10000)/100; // 퍼센트
 
-	var $progress_bar = $('.dday .progress-bar'),
-		$progress_text = $('.dday .progress-text'),
-		$dday_text = $('.dday .dday-text');
-	
-	$progress_text.text(Math.round(((a_dday - r_dday) / a_dday) * 10000)/100 + '%');
-	$progress_bar.css('width', ((r_dday <= 0) ? 100 : ((a_dday - r_dday) / a_dday) * 100) + '%');
-	$dday_text.text('D' + ((r_dday <= 0) ? '+' : '') + (-r_dday) + '(' + r_dday + ' / ' + a_dday + '일)');
+	$('.dday .progress-bar').css('width', per >= 100 ? '100%' : per + '%'),
+	$('.dday .progress-text').text(per >= 100 ? '전역!' : per + '%'),
+	$('.dday .dday-text').text('D' + ((r_dday <= 0) ? '+' : '') + (-r_dday) + '(' + r_dday + ' / ' + a_dday + '일)');
 }(jQuery));
