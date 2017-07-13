4332d0c57a8fe836d800dff8335eb1e461ddb8de
Inseop Kim <opnayn@gmail.com>
2017-07-13 20:16:36 +0900

    js: Add window.resize.js

---
+++ js/window.resize.js
@@ -0,0 +1,25 @@
+(function ($) {
+	let $win = $(window);
+	var resizeWin = function () {
+			let $body = $('body'), width = $win.width();
+			if ((width >= 1024) && !$body.hasClass('desktop')) {
+				console.log('Change Window size to Desktop');
+				$body.addClass('desktop').removeClass('tablet mobile');
+			} else if ((width >= 768) && (width < 1024) && !$body.hasClass('tablet')) {
+				console.log('Change Window size to Tablet');
+				$body.addClass('tablet').removeClass('desktop mobile');
+			} else if ((width < 768) && !$body.hasClass('mobile')) {
+				console.log('Change Window size to Mobile');
+				$body.addClass('mobile').removeClass('desktop tablet');
+			}
+
+			if ((width >= 1300) && !$body.hasClass('wide')) {
+				$body.addClass('wide');
+			} else if ((width < 1300) && $body.hasClass('wide')) {
+				$body.removeClass('wide');
+			}
+		};
+
+	// Add resize trigger and Run when load document
+	$win.resize(resizeWin); resizeWin();
+})(jQuery);
