6e9480a9011e1e2c1ca34aeb1979982e911ce673
Kim In Seop <opnayn@gmail.com>
2017-07-25 14:02:54 +0000

    index: Update dday

--- js/dday.js
+++ js/dday.js
@@ -7,7 +7,6 @@
 		a_dday = Math.ceil((dday.getTime() - sday.getTime()) / ytime), // 총 복무일
 		per = Math.round(((a_dday - r_dday) / a_dday) * 10000)/100; // 퍼센트
 
-	$('.dday .progress-bar').css('width', per >= 100 ? '100%' : per + '%'),
-	$('.dday .progress-text').text(per >= 100 ? '전역!' : per + '%'),
-	$('.dday .dday-text').text('D' + ((r_dday <= 0) ? '+' : '') + (-r_dday) + ' / ' + a_dday);
+	$('.dday .progress-bar').css('width', per >= 100 ? '100%' : per + '%');
+	$('.dday .dday-text').text('D' + ((r_dday <= 0) ? '+' : '') + (-r_dday) + ' (' + (per >= 100 ? '100% 달성, 전역!' : per + '%') + ')');
 }(jQuery));
