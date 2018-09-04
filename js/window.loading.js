b3feec1ba70a1edb3137eae9d78e7a41c4331f26
Kim In Seop <opnayn@gmail.com>
2018-09-04 11:24:56 +0900

    _includes/loading.html: Move scripts to inline

--- js/window.loading.js
+++
@@ -1,6 +0,0 @@
-(function ($) {
-    'use strict';
-    var cdiv = function (cname) {return $('<div>',{'class':cname});};
-    $('body').append(cdiv('loader-bg bg-p').append(cdiv('loader-wrap').append(cdiv('loader'))));
-    window.onload = function () { $('.loader-bg').addClass('done'); };
-})(jQuery);
