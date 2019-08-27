ea7577936483700822a2d386f82510c0fbd39323
Kim In Seop <opnayn@gmail.com>
2019-08-27 22:58:10 +0900

    pages: Update Index
    Signed-off-by: Kim In Seop <opnayn@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,17 @@
-import React from 'react';
+import './index.css';
 
-export default () => <div>Hello world!</div>;
+import React from 'react';
+import Button from '../components/atoms/Button';
+
+export default () => (
+  <div className={'main'}>
+    <div className={'profile'}>
+      <img src="/images/profile.png" alt="profile" />
+      <p>OPNay</p>
+    </div>
+    <div className={'menu'}>
+      <Button href="blog">Blog</Button>
+      <Button href="https://github.com/opnay">Github</Button>
+    </div>
+  </div>
+);
