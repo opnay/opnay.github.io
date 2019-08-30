ad515b1fd8a89ef13f90bf2d0de0f81f49ef5064
Kim In Seop <opnayn@gmail.com>
2019-08-30 20:48:26 +0900

    Gatsby: Enable plugins and Add markdown configure
    - gatsby-source-filesystem
    - gatsby-transformer-remark
    - gatsby-remark-external-links
    
    - gatsby-node.js
      - onCreateNode
        - Create graphql data field for markdown path
      - createPages
        - Render markdown page using slug(onCreateNode) path
    
    Signed-off-by: Kim In Seop <opnayn@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+const projectDir = fs.realpathSync(process.cwd());
+const _ = (relPath) => path.resolve(projectDir, relPath);
+
 module.exports = {
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: 'post',
+        path: _('blog'),
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-external-links',
+            options: {
+              target: '_blank',
+              rel: 'nofollow noopener noreferrer',
+            },
+          },
+          // 'gatsby-remark-prismjs',
+          // 'gatsby-remark-autolink-headers',
+        ],
+      },
+    },
     {
       resolve: `gatsby-plugin-typescript`,
       options: {
---
+++ gatsby-node.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const { createFilePath } = require(`gatsby-source-filesystem`);
+const _ = (...paths) => path.resolve(process.env.PWD, ...paths);
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  // Add slug
+  const { createNodeField } = actions;
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` });
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    });
+  }
+};
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  const templates = _('src/templates');
+  const result = await graphql(
+    `
+      {
+        allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+          nodes {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+              template
+            }
+          }
+        }
+      }
+    `
+  );
+
+  if (result.errors) {
+    throw result.errors;
+  }
+
+  // Create blog posts pages.
+  const posts = result.data.allMarkdownRemark.nodes;
+
+  posts.forEach((post, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1];
+    const next = index === 0 ? null : posts[index - 1];
+
+    createPage({
+      path: post.fields.slug,
+      component: _(templates, post.frontmatter.template + '.tsx'),
+      context: {
+        slug: post.fields.slug,
+        previous,
+        next,
+      },
+    });
+  });
+};
