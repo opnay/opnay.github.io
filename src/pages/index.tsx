4c9d2b6792ea4be5785db58a369af42a32161557
Kim In Seop <opnayn@gmail.com>
2019-09-18 04:14:26 +0900

    pages: index: Notice 'Current Working...'
    Signed-off-by: Kim In Seop <opnayn@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,9 @@ export default () => (
       <img src="/images/profile.png" alt="profile" />
       <p>OPNay</p>
     </div>
+    <Button className={'desc'} href="/Working">
+      🧰 현재 공사중입니다...
+    </Button>
     <div className={'menu'}>
       <Button href="blog">Blog</Button>
       <Button href="https://github.com/opnay">Github</Button>
