eafeff8ad01edbb03e52e785958186002d1f9183
Kim In Seop <opnayn@gmail.com>
2019-09-24 21:55:10 +0900

    atoms/Link: Fix double navigate when href is app address.
    Signed-off-by: Kim In Seop <opnayn@gmail.com>

--- src/components/atoms/Link/index.tsx
+++ src/components/atoms/Link/index.tsx
@@ -1,6 +1,6 @@
 import './styles.css';
 
-import React, { useMemo, useCallback } from 'react';
+import React, { useMemo, useCallback, MouseEvent } from 'react';
 import { navigate } from '@reach/router';
 
 type Props = React.DetailedHTMLProps<
@@ -8,23 +8,27 @@ type Props = React.DetailedHTMLProps<
   HTMLAnchorElement
 >;
 
+const isExternalHref = (href: string) => /^https?:\/\//g.test(href);
+
 const Link: React.FC<Props> = (props: Props) => {
   const { children, className, onClick, href, ...buttonProps } = props;
   const cn = useMemo(() => ['link', className].join(' '), [className]);
 
   // Enable href
   const onClickHref = useCallback(
-    (e) => {
+    (e: MouseEvent<HTMLAnchorElement>) => {
       if (!!onClick) {
         onClick(e);
       }
 
-      if (!!href) {
-        navigate(href);
+      // We use `navigate` of @reach/router
+      // instead of anchor's href attribute for PWA.
+      if (!isExternalHref(href)) {
+        // Disable href function
+        e.preventDefault();
 
-        // We use `navigate` of @reach/router
-        // instead of anchor's href attributes for PWA.
-        return false;
+        // Navigate to PWA page.
+        navigate(href);
       }
     },
     [onClick, href, navigate]
