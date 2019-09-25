65c48f41281a2b1756edf36c71deb747f3e12d35
Kim In Seop <opnayn@gmail.com>
2019-09-26 02:43:56 +0900

    pages/index: Use Layout and Rename index.css to styles.css
    Signed-off-by: Kim In Seop <opnayn@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,26 @@
-import './index.css';
+import './styles.css';
 
 import React from 'react';
 import Link from '../components/atoms/Link';
+import Layout from '../components/atoms/Layout';
 
 const MainPage = () => (
-  <div className={'main-page'}>
-    <div className={'profile'}>
-      <img src="/images/profile.png" alt="profile" />
-      <p>OPNay</p>
+  <Layout>
+    <div className={'main-page'}>
+      <div className={'profile'}>
+        <img src="/images/profile.png" alt="profile" />
+        <p>OPNay</p>
+      </div>
+      <Link className={'desc'} href="/Working">
+        🧰 현재 공사중입니다...
+      </Link>
+      <div className={'menu'}>
+        <Link href="https://blog.opnay.com">Tistory</Link>
+        <Link href="blog">Blog</Link>
+        <Link href="https://github.com/opnay">Github</Link>
+      </div>
     </div>
-    <Link className={'desc'} href="/Working">
-      🧰 현재 공사중입니다...
-    </Link>
-    <div className={'menu'}>
-      <Link href="https://blog.opnay.com">Tistory</Link>
-      <Link href="blog">Blog</Link>
-      <Link href="https://github.com/opnay">Github</Link>
-    </div>
-  </div>
+  </Layout>
 );
 
 export default MainPage;
