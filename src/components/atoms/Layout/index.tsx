51de3e4b3c23780bbfd4de73365b403a692183db
Kim In Seop <opnayn@gmail.com>
2019-09-26 02:47:31 +0900

    atoms/Layout: Make div component instead of Fragment
    Signed-off-by: Kim In Seop <opnayn@gmail.com>

--- src/components/atoms/Layout/index.tsx
+++ src/components/atoms/Layout/index.tsx
@@ -1,7 +1,23 @@
 import './styles.css';
 
-import { Fragment } from 'react';
+import React, { useMemo } from 'react';
 
-const Layout = Fragment;
+type Props = React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+> & {
+  children: React.ReactNode;
+};
+
+const Layout: React.FC<Props> = (props) => {
+  const { children, className, ...divProps } = props;
+  const cn = useMemo(() => ['layout', className].join(' '), [className]);
+
+  return (
+    <div {...divProps} className={cn}>
+      {children}
+    </div>
+  );
+};
 
 export default Layout;
