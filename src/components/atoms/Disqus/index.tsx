657d3d3981c1ef70f132aaec22b2761ff612e428
Kim In Seop <opnayn@gmail.com>
2019-10-09 20:20:16 +0900

    atoms/Disqus: Fix document is not defined when build.
    - Occured by webpack
    
    Signed-off-by: Kim In Seop <opnayn@gmail.com>

--- src/components/atoms/Disqus/index.tsx
+++ src/components/atoms/Disqus/index.tsx
@@ -4,22 +4,32 @@ import React, { useEffect } from 'react';
 
 type Props = {};
 
-const script = document.createElement('script');
-const timestamp = new Date().getTime().toString();
-const disqusID = `disqus-${timestamp}`;
+const useDisqus = (() => {
+  if (typeof document !== 'undefined') {
+    const script = document.createElement('script');
+    const timestamp = new Date().getTime().toString();
+    const disqusID = `disqus-${timestamp}`;
 
-const useDisqus = () => {
-  useEffect(() => {
-    if (!document.getElementById(disqusID)) {
-      script.id = disqusID;
-      script.src = 'https://opnay.disqus.com/embed.js';
-      script.setAttribute('data-timestamp', timestamp);
-      document.head.appendChild(script);
-    } else {
-      window['DISQUS'].reset({ reload: true });
-    }
-  }, []);
-};
+    return () => {
+      useEffect(() => {
+        if (!document.getElementById(disqusID)) {
+          script.id = disqusID;
+          script.src = 'https://opnay.disqus.com/embed.js';
+          script.setAttribute('data-timestamp', timestamp);
+          document.head.appendChild(script);
+        } else {
+          window['DISQUS'].reset({ reload: true });
+        }
+      }, []);
+    };
+  } else {
+    return () => {
+      useEffect(() => {
+        console.error(`There's no document`);
+      }, []);
+    };
+  }
+})();
 
 const Disqus: React.FC<Props> = () => {
   useDisqus();
