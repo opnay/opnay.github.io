5b0d1fc047626bbf40c270b7202ad230807bbf29
Kim In Seop <opnayn@gmail.com>
2019-10-09 23:39:13 +0900

    Setup Github Action (#4)
    Add Github Actions to deploy

---
+++ .github/scripts/deploy.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+###
+# Origin script
+# https://github.com/maxheld83/ghpages/blob/master/entrypoint.sh
+###
+
+# REQUIRE ENV
+# - BUILD_DIR
+# - TOKEN (Github Personal Access Token)
+# - CNAME
+
+set -e
+
+echo "=========="
+echo "Move to 'BUILD_DIR'"
+echo "- $BUILD_DIR"
+cd $BUILD_DIR
+
+echo "=========="
+echo "Git config"
+REPO_URI="https://${TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+git init && \
+git config user.name "${GITHUB_ACTOR}" && \
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
+if [ -z "$(git status --porcelain)" ]; then
+    echo "Nothing to commit" && \
+    exit 0
+fi
+
+echo "=========="
+echo "Fetch and soft checkout"
+git remote add origin $REPO_URI && \
+git fetch origin && \
+git reset --soft origin/master
+
+echo "=========="
+echo "Commit files"
+echo $CNAME > ./CNAME
+git add . && \
+git commit -m 'Deploy to GitHub Pages' && \
+git push origin master && \
+rm -fr .git && \
+cd $GITHUB_WORKSPACE
+
+echo "=========="
+echo "Done"
